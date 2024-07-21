26aca9687092727f99edcfcabe4d429f0ea93284
BoanergesJunior <boanerges_junior@hotmail.com>
2024-07-21 14:44:59 -0300

    EKS setted up

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,29 @@
 variable "prefix" {
-  type = string
+  type    = string
   default = ""
 }
 
 variable "cluster_name" {
-  type = string
+  type    = string
   default = ""
+}
+
+variable "retention_days" {
+  type    = number
+  default = 7
+}
+
+variable "desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "max_size" {
+  type    = number
+  default = 4
+}
+
+variable "min_size" {
+  type    = number
+  default = 2
 }
